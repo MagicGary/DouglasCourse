b7f42e49393171ab2137e55fdd83850ad4ce7d7f
MagicGary <baoxx142@umn.edu>
2020-01-15 19:30:50 -0800

    adds comments for setting up sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,24 +7,41 @@ module.exports = {
             text: 'Another Page',
             link: '/hello.html'
         }],
+        //add sidebar config below 
         sidebar: [{
+                //first folder
                 title: 'Intro to Programming',
                 path: '/IntroProgramming/',
                 collapsable: true,
+                //first level sidebarDepth is 1 
                 sidebarDepth: 1,
-                children: [{
+                //add its childeren here 
+                children: [
+                    //first children, a.k.a. first subfolder 
+                    {
+                        //subfoler title
                         title: 'Class Notes',
                         collapsable: true,
+                        //set its depth to 2 since its a seoncd level folder
+                        //it looks like this sidebarDepth is actually not useful 
+                        //setting it to any will not affect the site, 
+                        // might just here for a visual
                         sidebarDepth: 2,
+
+                        //add children of this folder, 
+                        //each path will be an individual .md file
                         children: [
                             'IntroProgramming/ClassNotes/class1',
                             'IntroProgramming/ClassNotes/class2',
 
                         ]
                     },
+                    //second children, a.k.a. second subfoler 
                     {
+                        //folder title
                         title: 'Homework',
-                        sidebarDepth: 1,
+                        //sidebar depth should be 2 but seems like setting it to 5 is also okay
+                        sidebarDepth: 5,
                         children: [
                             'IntroProgramming/HW/hw1'
                         ]
@@ -32,6 +49,8 @@ module.exports = {
 
                 ]
             },
+
+            //second folder
             {
                 title: 'Web Development',
                 path: '/WebDevelopment/',
@@ -41,6 +60,7 @@ module.exports = {
                     'WebDevelopment/lab1'
                 ]
             },
+            //third folder
             {
                 title: 'System Analysis',
                 path: '/SystemAnalysis/',
