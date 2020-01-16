2873beae13260171e68620dcfd140bf6e1fc674b
MagicGary <baoxx142@umn.edu>
2020-01-15 19:07:38 -0800

    added some packags

---
+++ docs/.vuepress/oktaConfig.js
@@ -0,0 +1,6 @@
+export const oktaConfig = {
+    issuer: "https://{https://dev-235278.okta.com/oauth2/default",
+    client_id: "0oawuhacT7E20IpaM4x5",
+    redirect_uri: "http://localhost:8080/implicit/callback/",
+    scope: "openid profile email"
+};
